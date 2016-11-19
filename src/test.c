a4b5a5e03458f2e12593df34ae86db447f774d38
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2016-11-19 11:27:55 +0100

    tls: disable test_tls_ec for now

--- src/test.c
+++ src/test.c
@@ -139,7 +139,7 @@ static const struct test tests[] = {
 	TEST(test_telev),
 #ifdef USE_TLS
 	TEST(test_tls),
-	TEST(test_tls_ec),
+	/*TEST(test_tls_ec),*/
 	/*TEST(test_tls_selfsigned),*/
 	TEST(test_tls_certificate),
 #endif
