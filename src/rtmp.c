7bdaf1a2ffaf6b602756d0106734ce62d752208c
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2019-05-24 22:00:49 +0200

    update mock ECDSA certificate
    - use common name = 127.0.0.1 so that the certificate can be
      used for tls verification

--- src/rtmp.c
+++ src/rtmp.c
@@ -663,7 +663,7 @@ static struct rtmp_endpoint *rtmp_endpoint_alloc(enum mode mode,
 #ifdef USE_TLS
 		char path[256];
 
-		re_snprintf(path, sizeof(path), "%s/rtmp.pem",
+		re_snprintf(path, sizeof(path), "%s/server-ecdsa.pem",
 			    test_datapath());
 
 		err = tls_alloc(&ep->tls, TLS_METHOD_SSLV23,
